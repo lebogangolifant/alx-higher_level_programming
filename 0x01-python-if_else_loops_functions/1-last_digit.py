3bb7389da6389d616739d065484fb280f9bfbe0b
Lebogang <lebogangodeveloper@gmail.com>
2023-06-06 15:18:46 +0200

    Fix formatting issue for displaying last digit as a string

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
-message = f"Last digit of {number:d} is {last_digit:d} and is "
+
+message = f"Last digit of {number:d} is {str(last_digit)} and is "
+
 if last_digit > 5:
     message += "greater than 5"
 elif last_digit == 0:
     message += "0"
 else:
     message += "less than 6 and not 0"
+
 print(message)
