327e5114369580dcfba4e6570f467927a072e6da
Lebogang <lebogangodeveloper@gmail.com>
2023-06-06 15:44:45 +0200

    Fix code to use only one print function with string format

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 ascii_value = 97
+output = ""
 while ascii_value <= 122:
-    print("{}".format(chr(ascii_value)), end="")
+    output += "{}".format(chr(ascii_value))
     ascii_value += 1
-print()
+
+print(output)
