90d5d5b63875ac8b0811c12f8fd1f67b370b3304
Lebogang <lebogangodeveloper@gmail.com>
2023-06-07 09:34:30 +0200

    Remove extra newline character at the end of the output

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@ output = ''
 for ascii_value in range(ord('a'), ord('z') + 1):
     if chr(ascii_value) not in {'q', 'e'}:
         output += "{}".format(chr(ascii_value))
-print(output)
+print(output, end="")
