ad57e76eddee5f3d99f43308bcf363b48f44eaaf
Lebogang <lebogangodeveloper@gmail.com>
2023-06-07 10:20:52 +0200

    Fix the shebang line

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
 for ascii_value in range(122, 96, -1):
     if ascii_value % 2 == 0:
-        print("{:c}".format(ascii_value), end="")
+        print(chr(ascii_value), end="")
     else:
-        print("{:c}".format(ascii_value - 32), end="")
+        print(chr(ascii_value - 32), end="")
 
-print()
+print(end="")
