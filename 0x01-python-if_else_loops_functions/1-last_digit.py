974bc1faa215ebb576a5f5fafd00872e6672017f
Lebogang <lebogangodeveloper@gmail.com>
2023-06-07 09:19:46 +0200

    Assign a random signed number to the variable number each time it is executed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 import random
-
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
-
-random.seed(0)  # Set a specific seed value for reproducibility
-
-message = f"Last digit of {number} is {last_digit} and is "
-
-if last_digit > 5:
-    message += "greater than 5"
-elif last_digit == 0:
-    message += "0"
+digit = abs(number) % 10
+if number < 0:
+    digit = -digit
+print("Last digit of {} is {} and is ".format(number, digit), end="")
+if digit > 5:
+    print("greater than 5")
+elif digit == 0:
+    print("0")
 else:
-    message += "less than 6 and not 0"
-
-print(message)
+    print("less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-for number in range(100):
-    print("{:02d}".format(number), end="")
-    if number != 99:
-        print(", ", end="")
-print()
