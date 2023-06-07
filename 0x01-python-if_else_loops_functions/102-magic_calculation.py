f59c2a28bc4f06279e11f64416ecefeeb953b74d
Lebogang <lebogangodeveloper@gmail.com>
2023-06-07 10:31:29 +0200

    Modified to remove the unnecessary loop and fix the issue causing an extra line with added characters

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    for _ in range(3):
-        if a < b:
-            return c
-        elif b > c:
-            return a + b
-        else:
-            return a * b - c
+    if a < b:
+        return c
+    elif b > c:
+        return a + b
+    else:
+        return a * b - c
