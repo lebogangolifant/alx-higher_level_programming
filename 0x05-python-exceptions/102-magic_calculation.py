2c096f3554b567ace707bc0cf68a96f6be381c83
Lebogang <lebogangodeveloper@gmail.com>
2023-06-27 11:54:17 +0200

    Round the result value to two decimal

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python
+
 def magic_calculation(a, b):
     result = 0
 
     for index in range(1, 4):
         try:
             if index > a:
-                raise Exception("Too far")
+                raise ValueError("Too far")
             result += (a ** b) / index
-        except:
+        except ValueError:
             result = b + a
             break
 
-    return result
+    return round(result, 2)
