0b6d049da359a88a291a7ee489339673cc09d941
Lebogang <lebogangodeveloper@gmail.com>
2023-06-27 11:25:29 +0200

    Fix return value and error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,8 +3,8 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        sys.stdout.write("{:d}\n".format(value))
+        print("{:d}".format(value))
         return True
     except Exception as e:
-        sys.stderr.write("Exception: {}\n".format(e))
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
