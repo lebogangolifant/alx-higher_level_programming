48030f6b023f49cf52b587033e22112e9fec9ee8
Lebogang <lebogangodeveloper@gmail.com>
2023-07-02 11:16:07 +0200

    Implement a function, which prints a square using the '#' character.
    It handles cases where the size parameter is not an integer or less than 0, raising appropriate exceptions.

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Function that prints a square with the character #.
+
+"""
+
+
+def print_square(size):
+    """
+    Prints a square with the character #.
+
+    Args:
+        size (int): The size length of the square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+
+    """
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
