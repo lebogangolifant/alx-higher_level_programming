d6dd3a05a41e689fc4dc103a376b8011d65e2737
Lebogang <lebogangodeveloper@gmail.com>
2023-07-02 14:23:38 +0200

    Implement unit tests for the max_integer function
        In this commit, I added unit tests for the max_integer function in the 6-max_integer.py module. The unit tests are implemented using the unittest module and cover various test cases to ensure the correct behavior of the function.

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,7 +4,7 @@ Unittests for the max_integer function.
 """
 
 import unittest
-from max_integer import max_integer
+max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
