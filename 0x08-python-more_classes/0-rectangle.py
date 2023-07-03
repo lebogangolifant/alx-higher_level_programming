c88c8b8661f24ccbc8f4a13c7987287923949e60
Lebogang <lebogangodeveloper@gmail.com>
2023-07-03 11:22:14 +0200

    Implement empty class Rectangle that defines a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Empty class Rectangle that defines a rectangle:"""
+
+
+class Rectangle:
+    """
+    The Rectangle class represents a rectangle shape.
+
+    Attributes:
+    - No attributes are defined for this class.
+
+    Methods:
+    - No methods are defined for this class.
+
+    """
+
+    pass
