601364741c3e249d5c7537e8d5c6a2f6b662bd3a
Lebogang <lebogangodeveloper@gmail.com>
2023-07-10 11:39:56 +0200

    Check Class Inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Function that returns True if the object is an instance of a class that
+inherited (directly or indirectly) from the specified class ; otherwise False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """Returns True if obj is an instance of a subclass, False otherwise."""
+    return isinstance(obj, a_class) and type(obj) is not a_class
