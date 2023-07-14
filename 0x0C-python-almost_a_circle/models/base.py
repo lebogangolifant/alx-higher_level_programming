ed63e41048ca2b4fc07c8500d7423b2883ff6bb3
Lebogang <lebogangodeveloper@gmail.com>
2023-07-14 13:47:35 +0200

    Implement base class that manages id attributes

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that manages the id attribute for all other classes in the project"""
+
+class Base:
+    """Base class for managing id attribute"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Class constructor for Base"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
