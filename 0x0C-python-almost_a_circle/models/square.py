e337d068e596f66c1ece7e87c95ecdd4fdc844e3
Lebogang <lebogangodeveloper@gmail.com>
2023-07-14 15:54:11 +0200

    Update the Square instance with the given arguments

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -27,3 +27,18 @@ class Square(Rectangle):
         """Return a string representation of the Square instance"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                  self.width)
+
+    def update(self, *args, **kwargs):
+        """Update the Square instance with the given arguments"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
