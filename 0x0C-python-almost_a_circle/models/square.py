fda337ff6ad256539449b1291429f7de12220204
Lebogang <lebogangodeveloper@gmail.com>
2023-07-18 11:53:37 +0200

    Update the class Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """Module  class Square that inherits from Rectangle"""
-
-
 from models.rectangle import Rectangle
 
 
@@ -19,45 +17,51 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-        """Setter for size (same as width)"""
         self.width = value
         self.height = value
 
-    def __str__(self):
-        """Return a string representation of the Square instance"""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
-                                                 self.width)
-
     def update(self, *args, **kwargs):
-        """Update the Square instance with the given arguments"""
-        if args:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.size = args[1]
-            if len(args) >= 3:
-                self.x = args[2]
-            if len(args) >= 4:
-                self.y = args[3]
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+        """Update the Square instance with the given argument"""
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
     def to_dictionary(self):
         """Return a dictionary representation of the Square instance"""
         return {
-            'id': self.id,
-            'size': self.width,
-            'x': self.x,
-            'y': self.y
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
         }
 
-    @classmethod
-    def create_from_csv_row(cls, row):
-        """Create a Square instance from a CSV row"""
-        id, size, x, y = map(int, row)
-        return cls(size, x, y, id)
-
-    def to_csv_row(self):
-        """Return the CSV row representation of a Square instance"""
-        return [self.id, self.width, self.x, self.y]
+    def __str__(self):
+        """Return the representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
