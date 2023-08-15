8faf5a86c8036943414224168823ba944a7e5cea
Lebogang <lebogangodeveloper@gmail.com>
2023-08-15 07:16:00 -0700

    Display the max temperature of each state (ordered by State name).

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,7 +1,5 @@
 -- Import the provided table dump into hbtn_0c_0 database
-USE hbtn_0c_0c_0
-
-SELECT state, MAX(temperature_fahrenheit) AS max_temp
+SELECT state, MAX(value) AS max_temp
 FROM temperatures
 GROUP BY state
-ORDER BY state;
+ORDER BY state ASC;
