c2dd8da3e238544ac9364fd3320d2d16c3203e4c
Lebogang <lebogangodeveloper@gmail.com>
2023-08-15 06:37:32 -0700

    List all records with a score in the table

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- Lists all records of the table `second_table`
 -- of the database `hbtn_0c_0` in MySQL Server.
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT `score`, `name` FROM `second_table` WHERE `score` >= 10 ORDER BY `score` DESC;
