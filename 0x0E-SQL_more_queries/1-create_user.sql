50b6eb4333130e8ce559a3d8a9fae0bf9ae15f1c
Lebogang <lebogangodeveloper@gmail.com>
2023-08-16 15:38:00 +0200

    Create the MySQL server user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
 -- Creates a MySQL Server user `user_0d_1`
 -- Grant MySQL server privileges to user_0d_1
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
