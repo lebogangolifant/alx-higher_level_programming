f1dbed01f28b702459599e8c9b5c8c6e2d51414d
Lebogang <lebogangodeveloper@gmail.com>
2023-08-17 09:51:37 +0200

    List shows by their rating sum

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,7 @@
 -- Lists all shows by their rating.
 -- from the hbtn_0d_tvshows_rate database
-SELECT tv_shows.title, SUM(rating) AS rating_sum
+USE hbtn_0d_tvshows_rate;
+SELECT tv_shows.title, SUM(rating.rating) AS rating_sum
 FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
 GROUP BY tv_shows.title
