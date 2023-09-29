865ee65f0e2a403d5d912453e33ea37316c59564
Lebogang <lebogangodeveloper@gmail.com>
2023-09-29 14:12:00 +0200

    Fetch the URL using the requests package and displays the body of the response

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     if response.status_code == 200:
         body = response.text
         print("Body response:")
-        print(f"    - type: {type(body)}")
-        print(f"    - content: {body}")
+        print("\t- type: {}".format(type(response.text)))
+        print("\t- content: {}".format(response.text))
     else:
         print(f"Error: {response.status_code}")
