e213989285c8d6a6077e162469cde30a7e72723c
Lebogang <lebogangodeveloper@gmail.com>
2023-09-29 05:54:40 -0700

    Handle HTTP status codes

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
     response = requests.get(url)
 
     if response.status_code >= 400:
-        print("Error code:{}".format(response.status_code))
+        print("Error code: {}".format(response.status_code))
     else:
         print(response.text)
