16eee513b1c6c8c26f305dc3a93a6ac83c8dc615
Lebogang <lebogangodeveloper@gmail.com>
2023-09-29 13:11:05 +0200

    Sends a POST request to the specified URL with the email as a parameter

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = {'email': email}
+
+    response = requests.post(url, data=data)
+
+    sys.stdout.write(response.text)
