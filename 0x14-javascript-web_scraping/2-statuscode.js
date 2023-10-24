76a7d979f5a17af8ff7ab2081886339832137420
Lebogang <lebogangodeveloper@gmail.com>
2023-10-24 14:40:50 +0200

    Display the status code of a GET request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**
+ * Display the status code of a GET request
+ */
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log('code:', response && response.statusCode);
+  } else {
+    console.log('code:', response && response.statusCode);
+  }
+});
