f05a22147fd11660f2f38577fa3159292f13e0d3
Lebogang <lebogangodeveloper@gmail.com>
2023-11-01 12:38:12 +0200

    Fetch and lists the title for all movies from a URL: https://swapi-api.alx-tools.com/api/films/?format=json

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,8 @@
+// Fetches and lists the title for all movies by using a URL
+
+/* global $ */
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+  data.results.forEach(function (movie) {
+    $('#list_movies').append('<li>' + movie.title + '</li>');
+  });
+});
