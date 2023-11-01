2302d7cbc2fccbf92982364f5ab0242d1327cc89
Lebogang <lebogangodeveloper@gmail.com>
2023-11-01 14:15:27 +0200

    Fetch and print text translation based on the language code entered by the user

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,20 @@
+// Fetches and prints how to say “Hello” depending on the language
+
+/* global $ */
+$(document).ready(function() {
+  $('#btn_translate').click(function() {
+    const languageCode = $('#language_code').val();
+    const apiUrl = 'https://www.fourtonfish.com/hellosalut/hello/?lang=' + languageCode;
+
+    $.ajax({
+      url: apiUrl,
+      type: 'GET',
+      success: function(data) {
+        $('DIV#hello').text(data.hello);
+      },
+      error: function() {
+        $('DIV#hello').text('Error: Language code not found');
+      }
+    });
+  });
+});
