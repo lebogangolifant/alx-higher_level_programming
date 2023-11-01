62e91e0c62454bc7aba11e19a5ac0d3a254d2f50
Lebogang <lebogangodeveloper@gmail.com>
2023-11-01 15:06:45 +0200

    Fetch and display the text translation based on the language code entered by the user, and to trigger the translation when the user clicks, the "Translate" button or presses Enter in the input field

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,33 @@
+// Fetches and prints how to say “Hello” depending on the language
+
+/* global $ */
+$(document).ready(function () {
+  // Attach click event handler to the Translate button
+  $('#btn_translate').click(translateHello);
+
+  // Function to fetch and display the translation
+  function translateHello () {
+    const languageCode = $('#language_code').val();
+    const apiUrl = `https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`;
+
+    // Send a GET request using the Fetch API
+    fetch(apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.code && data.code === 'none') {
+          $('#hello').text('Language code not found');
+        } else {
+          $('#hello').text(data.hello);
+        }
+      })
+      .catch(error => {
+        $('#hello').text('Error: Language code not found');
+        console.error(error);
+      });
+  }
+});
